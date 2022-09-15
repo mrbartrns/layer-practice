33ed3aa89927a011ab1ca78deebe37dbd5b72a4e
Junhyuck Ko <mrbartrns@naver.com>
2022-09-15 14:25:47 +0900

    feat: Post 클래스 구현

---
+++ src/services/Post/Post.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-underscore-dangle */
+import { PostResponse } from '@/api/post';
+
+class Post {
+  get UserId() {
+    return this.userId;
+  }
+
+  get Id() {
+    return this.id;
+  }
+
+  get Title() {
+    return this.title;
+  }
+
+  get Body() {
+    return this.body;
+  }
+
+  constructor(initialData: PostResponse) {
+    this.userId = initialData.userId;
+    this.id = initialData.id;
+    this.title = initialData.title;
+    this.body = initialData.body;
+  }
+
+  private readonly userId: number;
+
+  private readonly id: number;
+
+  private readonly title: string;
+
+  private readonly body: string;
+}
+
+export default Post;
