bfaec2186e5ab95882bbf251e4de5b37592e937c
Junhyuck Ko <mrbartrns@naver.com>
2022-09-15 16:16:12 +0900

    chore: 필요 없는 파일 제거

--- src/services/Post/Post.ts
+++ src/services/Post/Post.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle */
 import { PostResponse } from '@/api/post';
 
 class Post {
--- src/services/Post/index.ts
+++ src/services/Post/index.ts
@@ -1,2 +1,3 @@
-export { default as Post } from './Post';
-export { default as PostList } from './PostList';
+import Post from './Post';
+
+export default Post;
